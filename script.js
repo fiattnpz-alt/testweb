aafde7484fc04f44af88e2e6a6071f3fc74a9b07
fiattnpz-alt <fiattnpz@gmail.com>
2026-01-05 13:31:29 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         data.lineUserId = lineUserId;
 
         // REPLACEME: User needs to replace this URL after deploying their script
-        const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby2fU9O2n5COO72kyVSZxE1UdXsMeI6efQGTb8LiOuKR9szks4VQ4h2fk40wcfvtz0/exec';
+        const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxBhIvL8qcHT0MawMxwCIgHdy9309JWkpdKsewZk2McldiCg-zXZ31pOgMYmbFj74c/exec';
 
         fetch(GOOGLE_SCRIPT_URL, {
             method: 'POST',
